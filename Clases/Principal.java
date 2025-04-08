4217a3e1756139ab63400326b04b6c182f97801d
Nayane-snores <67526950+Nayane-snores@users.noreply.github.com>
2025-04-08 11:46:06 -0500

    Update Principal.java
    ya esta falta crear objeto cliente, supervisor y generar un reporte

--- Clases/Principal.java
+++ Clases/Principal.java
@@ -13,7 +13,7 @@ public class Principal{
 		
 		System.out.println(nuevo.toString());
 		
-		Producto otro = new Producto(1, "galleta", "es salada", "0001111", LocalDate.of(2025, 1,2), LocalDate.of(2030, 1, 4), 5.44, 142, 100);
+		Producto otro = new Producto(1, "agua", "cara", "0001111", LocalDate.of(2025, 1,2), LocalDate.of(2030, 1, 4), 5.44, 142, 100);
 		
 		System.out.println(otro.toString());
 		
@@ -35,7 +35,8 @@ public class Principal{
 		
 		
 		//crear unas 2 frutas
-		Fruta platano = new Fruta(); 
+		Fruta platano = new Fruta(1, "batidoFresa", "yOtras", "0001123", LocalDate.of(2025, 1,2), LocalDate.of(2030, 5, 4), 10.2, 20, 50,
+		5, true, true,false, false, "plastico"); 
 		Fruta fresa = new Fruta(); 
 
 		//agregamos frutas		
@@ -46,18 +47,32 @@ public class Principal{
 		System.out.println(b.toString());
 		
 		//crear un empleado 
-		
+		Repartidor frutiRepartidor = new Repartidor("Enzo", "Avila", "Mamani",
+		"a20220123@pucp.edu.pe", "+51 984 321 122", LocalDate.now(),
+		1040.5, "EnzitoEmoxito", "VIVAESPANA3"); 
 		
 		//crear una orden de venta 
+		OrdenVenta primera = new OrdenVenta("Mi primera orden");   
+		
+		//crear 3 linea orden de venta y asociarlas a la orden de venta -- estamos creando propias instancias de producto no asociando los 
+		//creados anterioremente
+		primera.agregarLineaOrden(new LineaOrdenDeVenta(1, 3, otro));
+		primera.agregarLineaOrden(new LineaOrdenDeVenta(2, 5, a));
+		primera.agregarLineaOrden(new LineaOrdenDeVenta(3, 1, b));
+		
+		ComprobantePago primerComprobante = primera.crearComprobantePago(); 
+		
+		//probar que el Empleado/repartidor chambea y termina la orden de venta
+		frutiRepartidor.prepararPedido(primera);
+		
+		frutiRepartidor.confirmarEntregaCliente(primera, false);
 		
 		
-		//crear 3 linea orden de venta 
+		//Le dejo a alguien mas terminar cliente, supervisor y generar reportes a ver si no hay error :v
 		
 		//crear un cliente 
 		
-		
-		//crear un supervisor 
-		
+		//crear un supervisor
 		
 		//imprimir reportes
 		
