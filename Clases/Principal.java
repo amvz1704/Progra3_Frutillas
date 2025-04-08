e51e0a1726fabc11db2c0da2ee146a10f4d0d6d9
amvz1704 <amvz17042013@gmail.com>
2025-04-07 22:19:00 -0500

    Principal

---
+++ Clases/Principal.java
@@ -0,0 +1,65 @@
+
+//agregar lo hecho en producto
+import java.time.*; 
+
+public class Principal{
+
+	public static void main(String[] args){
+		
+		//1. Creamos productos 
+		
+		// some code here in the main() method
+		Producto nuevo = new Producto(); 
+		
+		System.out.println(nuevo.toString());
+		
+		Producto otro = new Producto(1, "galleta", "es salada", "0001111", LocalDate.of(2025, 1,2), LocalDate.of(2030, 1, 4), 5.44, 142, 100);
+		
+		System.out.println(otro.toString());
+		
+		
+		//crear un snack 
+		
+		Snack a = new Snack(1, "cereal", "dulce", "0001112", LocalDate.of(2025, 1,2), LocalDate.of(2030, 5, 4), 1.44, 56, 50,
+		1, "tipoIntegral","pequenio", true,true);
+		
+		System.out.println(a.toString());
+		
+		
+		//crear una bebida pregunta cual es el stock de bebidas? 
+		Bebida b = new Bebida(1, "batidoFresa", "yOtras", "0001123", LocalDate.of(2025, 1,2), LocalDate.of(2030, 5, 4), 10.2, 20, 50,
+		2, 12, "batido", 
+		"rubia", tipoLeche.ENTERA);
+		
+		System.out.println(b.toString());
+		
+		
+		//crear unas 2 frutas
+		Fruta platano = new Fruta(); 
+		Fruta fresa = new Fruta(); 
+
+		//agregamos frutas		
+		
+		b.agregarFruta(platano);
+		b.agregarFruta(fresa); 
+		
+		System.out.println(b.toString());
+		
+		//crear un empleado 
+		
+		
+		//crear una orden de venta 
+		
+		
+		//crear 3 linea orden de venta 
+		
+		//crear un cliente 
+		
+		
+		//crear un supervisor 
+		
+		
+		//imprimir reportes
+		
+	}
+}
