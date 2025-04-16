138c684696df0211ca5be9fdbec0670e8dffd572
regSan17 <a20221221@pucp.edu.pe>
2025-04-16 12:19:47 -0500

    Delete Clases directory
    todos los archivos correctos estan en proyecto

--- Clases/FormaDePago.java
+++
@@ -1,5 +0,0 @@
-package com.frutilla.models.Venta;
-
-public enum FormaDePago{
-    TARJETA_CREDITO, TARJETA_DEBITO, PLIN, YAPE
-}
--- Clases/tipoLeche.java
+++
@@ -1,7 +0,0 @@
-package com.frutilla.models.Inventario;
-
-//en java automaticamente es null un enum
-
-public enum tipoLeche{
-	ENTERA, SIN_LACTOSA
-}
