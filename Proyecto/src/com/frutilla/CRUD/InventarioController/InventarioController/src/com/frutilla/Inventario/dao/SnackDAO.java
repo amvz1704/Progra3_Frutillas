33d78716451196c51433750013578b194681a784
regSan17 <a20221221@pucp.edu.pe>
2025-04-19 18:35:47 -0500

    Proyecto Java: InventarioController
    Aqui esta los dao y los mysql de inventario

---
+++ Proyecto/src/com/frutilla/CRUD/InventarioController/InventarioController/src/com/frutilla/Inventario/dao/SnackDAO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.frutilla.Inventario.dao;
+
+/**
+ *
+ * @author Desktop
+ */
+import com.frutilla.models.Inventario.Snack;
+import java.util.ArrayList;
+import java.sql.SQLException;
+
+
+public interface SnackDAO {
+     int insertar(Snack snack,int idLocal) throws SQLException;
+     int actualizarEnvasado(int idProducto,boolean envasado,String envase,
+             int idLocal) throws SQLException;
+     Snack obtenerDatosSnack(int idProducto,int idLocal) throws SQLException;
+     ArrayList<Snack> obtenerTodos(int idLocal) throws SQLException;
+}
