bd28fe542bb71840d2948094e84dd32d923a7595
JumiHV <jumihv.22.04.03@gmail.com>
2025-04-18 23:31:26 -0500

    DBManager

--- Proyecto/src/com/frutilla/config/DBManager.java
+++ Proyecto/src/com/frutilla/config/DBManager.java
@@ -2,36 +2,42 @@ package com.frutilla.config;
 
 import java.sql.DriverManager;
 import java.sql.Connection;
+import java.util.Properties;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.SQLException;
 
 public class DBManager {
-    
-    private Connection con;
-    private static DBManager dbManager;
-    private final String url = "jdbc:mysql://<host>:3306/<db_name>";
-    private final String username = "admin";
-    private final String password = "labs1inf3020241";
-    
-    @SuppressWarnings("UseSpecificCatch")
-    public Connection getConnection(){
-        try{
+    private static String url;
+    private static String user;
+    private static String password;
+
+    static{
+        String pathFile = "/com/frutilla/config/config.properties";
+        try(InputStream input = DBManager.class.getClassLoader().getResourceAsStream(pathFile)){
+            if(input == null){
+                System.out.println("No se encuentra el archivo: " + pathFile);
+            }
+            Properties prop = new Properties();
+            prop.load(input);
+            url = prop.getProperty("db.url");
+            user = prop.getProperty("db.user");
+            password = prop.getProperty("db.password");
+            if(url == null || user == null || password == null){
+                System.out.println("Faltan propiedades en el archivo: " + pathFile);
+            }
+
+            //Cargar el driver de MySQL
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(url, username, password);     
-        }catch(Exception ex){
-            System.out.println(ex.getMessage());
         }
-        return con;
+        catch(Exception e){
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
-    
-    public static DBManager getInstance(){
-        if(dbManager == null){
-            createInstance();
-        }
-        return dbManager;
-    }
-    
-    private synchronized static void createInstance(){
-        if(dbManager == null){
-            dbManager = new DBManager();
-        }
+
+    //Metodo estatico para obtener la conexion a la base de datos
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(url, user, password);
     }
 }
