5f06bc14e3eb000a7f5865ec54a57a04f29bca59
Nayane-snores <67526950+Nayane-snores@users.noreply.github.com>
2025-04-20 15:21:22 -0500

    CreaciondeMySQL_Local

---
+++ Proyecto/src/com/frutilla/crud/mysql/rrhh/LocalMySQL.java
@@ -0,0 +1,8 @@
+package com.frutilla.crud.mysql.local;
+
+
+import com.frutilla.crud.dao.local.LocalDAO; //incluimos la interfaz del local
+
+public class LocalMySQL implements LocalDAO{
+	//implementamos lol uwu 
+}
