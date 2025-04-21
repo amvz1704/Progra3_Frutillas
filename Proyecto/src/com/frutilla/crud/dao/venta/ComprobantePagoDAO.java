575cf6a28a14848bbaecbb42829d7efaef4830ae
MayLopez02 <mayerly02.lopez@gmail.com>
2025-04-20 23:44:14 -0500

    Add files via upload

---
+++ Proyecto/src/com/frutilla/crud/dao/venta/ComprobantePagoDAO.java
@@ -0,0 +1,25 @@
+
+package com.frutilla.crud.dao.venta;
+
+import com.frutilla.models.venta.ComprobantePago;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface ComprobantePagoDAO {
+
+    // Método para insertar un nuevo comprobante de pago
+    public void insertar(ComprobantePago comprobantePago) throws SQLException;
+
+    // Método para modificar un comprobante de pago
+    public void modificar(ComprobantePago comprobantePago) throws SQLException;
+
+    // Método para eliminar un comprobante de pago
+    public void eliminar(int idComprobante) throws SQLException;
+
+    // Método para obtener todos los comprobantes de pago
+    public ArrayList<ComprobantePago> obtenerTodos() throws SQLException;
+
+    // Método para obtener un comprobante de pago por su ID
+    public ComprobantePago obtenerPorId(int idComprobante) throws SQLException;
+}
+
