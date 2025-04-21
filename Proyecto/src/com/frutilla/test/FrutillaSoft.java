c166e5d04cc9e274660666f5cb3b05ca34e65ccf
Nayane-snores <67526950+Nayane-snores@users.noreply.github.com>
2025-04-20 20:54:09 -0500

    CambioAgregando

--- Proyecto/src/com/frutilla/test/FrutillaSoft.java
+++ Proyecto/src/com/frutilla/test/FrutillaSoft.java
@@ -29,31 +29,34 @@ public class FrutillaSoft{
 		try{
 			
 		/*B: Todo esto funciona correctamente uwu 
-			LocalDAO primerLocal = new LocalMySQL(); //instanciamos nuestro local
+			LocalDAO segundoLocal = new LocalMySQL(); //instanciamos nuestro local
 			//ahora creamos el objeto local 
-			Local localSociales = new Local("Sociales","Entre el polideportivo y generales ", "Estudios Generales Letras, Av. Universitaria 1801, San Miguel 15088", "999999999");
+			Local localA = new Local("Polideportivo","Cerca del polideportivo ", "CENTRUM PUCP, Av. Universitaria 1801, San Miguel 15088", "995-777-011");
 			
-			primerLocal.crearLocal(localSociales);
+			segundoLocal.crearLocal(localA);
 			
 			
 			//obtener el id del local no necesario pero para asegurarnos que el incremnet esta bien
-			int idLocal_Sociales = localSociales.getIdLocal(); //nota Nayane: solo funciona si las tablas en la BD tiene autoincrement 
+			int idLocal_Sociales = localA.getIdLocal(); //nota Nayane: solo funciona si las tablas en la BD tiene autoincrement 
 			System.out.println(idLocal_Sociales);
-		/**/	
+			
+		/*B: Todo esto funciona correctamente uwu */	
 			
 		/*A: Todo esto debajo funciona pero por x motivo se demora :'v --> 
 			EmpleadoDAO primerEmpleado = new EmpleadoMySQL(); 
 			EmpleadoDAO segundoEmpleado = new EmpleadoMySQL(); 
 			
 			//tenemos que crear un supervisor
-			Supervisor supervisor1 = new Supervisor("Nayane", "Melendez", "Saire", "nayane@gmail.com", "999999999", LocalDate.now() , 2000, "nayane", "1234"); 
+			Supervisor supervisor1 = new Supervisor("Naruto", "Uzumaki", "Namikaze", "hokageDeLaHoja@gmail.com", "999999993", LocalDate.now() , 2000, "6toHokage", "sasukei<3u"); 
 			
 			//Creamos un repartidor
-			Repartidor repartidor1 = new Repartidor("Enzo", "Avila", "Mamani", "enzo@gmail.com", "999999999", LocalDate.now(), 1000, "enzo", "1234");
+			Repartidor repartidor1 = new Repartidor("Itachi", "Uchiha", "Mamani", "uchihaKiller@gmail.com", "999999991", LocalDate.now(), 1066, "itachi", "contra1234");
 			
-			//Como dijo junior el idLocal lo asignamos al nuevo local creado owo 
-			primerEmpleado.insertarEmpleado(repartidor1, localSociales.getIdLocal());
-			segundoEmpleado.insertarEmpleado(supervisor1, localSociales.getIdLocal(); ); //esto es nuevo estamos agregando un supervisor al local de id 1 owo
+			//Como dijo junior el idLocal lo asignamos al nuevo local creado owo  --Side note: Supongo que dependiendo del local en el que se trabaje esto automaticamente hace el get del local
+			
+			
+			primerEmpleado.insertarEmpleado(repartidor1, localA.getIdLocal());
+			segundoEmpleado.insertarEmpleado(supervisor1, localA.getIdLocal()); //esto es nuevo estamos agregando un supervisor al local de id 1 owo
 		/*A: Todo esto debajo funcion --> */
 		
 			
