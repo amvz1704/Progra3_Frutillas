b9ed483903e912418904c715c9413467d72c38e2
MayLopez02 <mayerly02.lopez@gmail.com>
2025-04-21 11:15:13 -0500

    Update ComprobantePagoDAO.java

--- Proyecto/src/com/frutilla/crud/dao/venta/ComprobantePagoDAO.java
+++ Proyecto/src/com/frutilla/crud/dao/venta/ComprobantePagoDAO.java
@@ -8,18 +8,18 @@ import java.util.ArrayList;
 public interface ComprobantePagoDAO {
 
     // Método para insertar un nuevo comprobante de pago
-    public int insertar(ComprobantePago comprobantePago) throws SQLException;
+    int insertar(ComprobantePago comprobantePago) throws SQLException;
 
     // Método para modificar un comprobante de pago
-    public int modificar(ComprobantePago comprobantePago) throws SQLException;
+    int modificar(ComprobantePago comprobantePago) throws SQLException;
 
     // Método para eliminar un comprobante de pago
-    public int eliminar(int idComprobante) throws SQLException;
+    int eliminar(int idComprobante) throws SQLException;
 
     // Método para obtener todos los comprobantes de pago
-    public ArrayList<ComprobantePago> obtenerTodos() throws SQLException;
+    ArrayList<ComprobantePago> obtenerTodos() throws SQLException;
     
     // Método para obtener un comprobante de pago por su ID
-    public ComprobantePago obtenerPorId(int idComprobante) throws SQLException;
+    ComprobantePago obtenerPorId(int idComprobante) throws SQLException;
 }
 
