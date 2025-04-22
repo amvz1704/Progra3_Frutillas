352e48fd4e5dcf619149fcf49d28e4df363412cb
MayLopez02 <mayerly02.lopez@gmail.com>
2025-04-22 17:21:03 -0500

    Update Supervisor.java

--- Proyecto/src/com/frutilla/models/rrhh/Supervisor.java
+++ Proyecto/src/com/frutilla/models/rrhh/Supervisor.java
@@ -10,6 +10,9 @@ public class Supervisor extends Empleado{
     public Supervisor(String nombre, String apellidoPaterno, String apellidoMaterno, String correoElectronico, String telefono, LocalDate fechaContrato, double salario, String usuarioSistema, String contraSistema){
         super(nombre, apellidoPaterno, apellidoMaterno, correoElectronico, telefono, fechaContrato, salario, usuarioSistema, contraSistema, 'S');
     }
-    
+
+    public generarReporte () {
+        
+    }
 
 }
