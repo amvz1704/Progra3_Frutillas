ba46518f6f1cb0aea5544c6c7395bc3afe55068b
Nayane-snores <67526950+Nayane-snores@users.noreply.github.com>
2025-05-11 11:50:34 -0500

    xd

--- Frutilla/FrutillaPersistencia/src/main/java/pe/edu/pucp/frutilla/crud/mysql/local/LocalMySQL.java
+++ Frutilla/FrutillaPersistencia/src/main/java/pe/edu/pucp/frutilla/crud/mysql/local/LocalMySQL.java
@@ -24,144 +24,80 @@ import pe.edu.pucp.frutilla.config.DBManager; //El manager
 	
 
 public class LocalMySQL extends BaseDAOImpl<Local> implements LocalDAO{
+     //primero los tipo inter
+    @Override
+    protected String getInsertQuery() {
+        // Ajusta los nombres de columna a tu tabla real
+        return "INSERT INTO local (nombre, descripcion, direccion, telefono) VALUES (?, ?, ?, ?)";
+    }
     
     @Override
+    protected String getUpdateQuery() {
+        return "UPDATE local SET nombre = ?, descripcion= ?, direccion = ?, telefono = ? WHERE id_local = ?";
+    }
+
+    @Override
+    protected String getDeleteQuery() {
+        return "UPDATE Local SET activo = false WHERE idLocal = ?";
+    }
+  
+    @Override
+    protected String getSelectByIdQuery() {
+        return "SELECT id_local, nombre, direccion, telefono FROM local WHERE id_local = ?";
+    }
+
+    @Override
+    protected String getSelectAllQuery() {
+        return "SELECT * FROM Local WHERE activo = true";
+    }
+    //creacion de un local en base a un objeto "Local inicializado"
+    @Override
     protected void setInsertParameters(PreparedStatement ps, Local entity) throws SQLException{
-        ps
-        String query = "INSERT INTO Local (nombre, descripcion, direccion, activo, telefono) VALUES (?, ?, ?, ?, ?)";
-            try(Connection con = DBManager.getInstance().getConnection(); PreparedStatement ps = con.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS )){// Obtiene la conexion y prepara la consulta
-                    setLocalParameters(ps, local);// Establece los parámetros del local
-                    ps.executeUpdate();// Ejecuta la consulta
-                    try(ResultSet rs = ps.getGeneratedKeys()){// Obtiene las claves generadas por la consulta anterior
-                            if (rs.next()) {
-                                    local.setIdLocal(rs.getInt(1)); // Establece el ID en el objeto local
-                            }
-                    }     
-            }
-    
+        // Aquí van en orden los “?” de getInsertQuery()
+        
+        ps.setString(1, entity.getNombre());
+        ps.setString(2, entity.getDescripcion());
+        ps.setString(3, entity.getDireccion());
+        ps.setString(4, entity.getTelefono());
     } 
     //creacion de un local en base a un objeto "Local inicializado"
-   
-
-
-
-    public ArrayList<Local> obtenerTodosLocales() throws SQLException{
-            ArrayList<Local> locales = new ArrayList<Local>();
-
-    String query="SELECT * FROM Local WHERE activo = true"; //Consulta SQL para obtener todos los locales activos
-    try(Connection con=DBManager.getInstance().getConnection();
-        PreparedStatement ps=con.prepareStatement(query);
-                    ResultSet rs=ps.executeQuery()){
-
-                            while(rs.next()){
-                                    locales.add(mapLocal(rs));
-                            }  
+    
+    @Override
+    protected Local createFromResultSet(ResultSet rs) throws SQLException{
+        Local local = new Local(rs.getString("nombre"), rs.getString("descripcion"),
+                rs.getString("direccion"), rs.getString("telefono"));
+        local.setIdLocal(rs.getInt("idLocal")); //revisar si tiene este nombre
+        local.setActivo(rs.getBoolean("activo"));
+        
+        return local;
     }
-
-    return locales;
-
-
+    
+    @Override 
+    protected void setUpdateParameters(PreparedStatement ps, Local entity) throws SQLException{
+            ps.setString(1, entity.getNombre());
+            ps.setString(2, entity.getDescripcion());
+            ps.setString(3, entity.getDireccion());
+            ps.setBoolean(4, entity.getActivo());
+            ps.setString(5, entity.getTelefono());
     }
-
-
-
-
-
+    //
+    @Override
+    protected void setId(Local entity, Integer id) {
+        entity.setIdLocal(id);
+    }
+    
     //Devuelve una lista de empleados de un local por id --> llama a EmpleadoDAOSQL 
     public ArrayList<Empleado> encontrarEmpleados(int idLocal) throws SQLException{
 
         EmpleadoMySQL interfazEmpleado = new EmpleadoMySQL(); 
-
-        return interfazEmpleado.obtenerTodos(idLocal); 
+        return interfazEmpleado.listarTodos(idLocal); //debe implementar 
 
     }
 
     //Devuelve una lista de producto de un local por id --> llama a ProductosDAOSQL 
     public ArrayList<Producto> encontrarProductos(int idLocal) throws SQLException{
-
-            ProductoMySQL interfazProducto = new ProductoMySQL(); 
-
-            return interfazProducto.obtenerTodos(idLocal); 
-
-
-    }
-
-
-    public Local obtenerLocalPorId(int idLocal) throws SQLException{
-    String query = "SELECT * FROM Local WHERE idLocal = ?";
-        try(Connection con = DBManager.getInstance().getConnection(); PreparedStatement ps = con.prepareStatement(query)){
-            ps.setInt(1,idLocal);// Establece el ID del local en la consulta
-            try(ResultSet rs = ps.executeQuery()){
-                if(rs.next()){
-                    return mapLocal(rs);
-                }
-            }
-        }
-        return null;
-    }
-
-
-    private Local mapLocal(ResultSet rs) throws SQLException{
-
-    Local local = new Local(rs.getString("nombre"), rs.getString("descripcion"), rs.getString("direccion"), rs.getString("telefono"));
-
-    local.setIdLocal(rs.getInt("idLocal"));
-            local.setActivo(rs.getBoolean("activo"));
-
-
-    return local;
-}
-
-    private void setLocalParameters(PreparedStatement ps,Local local) throws SQLException {
-        // Establece los parámetros del empleado en la consulta SQL
-        ps.setString(1, local.getNombre());
-        ps.setString(2, local.getDescripcion());
-        ps.setString(3, local.getDireccion());
-        ps.setBoolean(4, local.getActivo());
-        ps.setString(5, local.getTelefono());
-    }
-
-    public void actualizarLocal(Local local) throws SQLException{
-        String query = "UPDATE Local SET nombre = ?, descripcion = ?, direccion = ?, activo = ?, telefono = ? WHERE idLocal = ?";
-        try(Connection con = DBManager.getInstance().getConnection(); PreparedStatement ps = con.prepareStatement(query)){
-            setLocalParameters(ps, local);// Establece los parámetros del local
-            //ps.setInt(6, local.getIdSupervisor());
-            ps.setInt(6, local.getIdLocal());
-            ps.executeUpdate();
-        }
-    }
-
-    public void eliminarLocalPorId(int idLocal) throws SQLException{
-        String query = "UPDATE Local SET activo = false WHERE idLocal = ?";
-        try(Connection con = DBManager.getInstance().getConnection(); PreparedStatement ps = con.prepareStatement(query)){
-            ps.setInt(1, idLocal);// Establece el ID del local en la consulta
-            ps.executeUpdate();// Ejecuta la consulta
-        }
-    }
-
-    @Override
-    public void agregar(Local entidad) {
         
+        ProductoMySQL interfazProducto = new ProductoMySQL(); 
+        return interfazProducto.obtenerTodos(idLocal); 
     }
-
-    @Override
-    public Local obtener(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public List<Local> listarTodos() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void actualizar(Local entidad) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void eliminar(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-	
 }
