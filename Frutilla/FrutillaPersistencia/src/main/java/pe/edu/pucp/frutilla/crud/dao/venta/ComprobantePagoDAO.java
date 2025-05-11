6771dccab4b1c30a6675c88a03d24f7528341d31
MayLopez02 <mayerly02.lopez@gmail.com>
2025-05-11 16:40:30 -0500

    Update ComprobantePagoDAO.java

--- Frutilla/FrutillaPersistencia/src/main/java/pe/edu/pucp/frutilla/crud/dao/venta/ComprobantePagoDAO.java
+++ Frutilla/FrutillaPersistencia/src/main/java/pe/edu/pucp/frutilla/crud/dao/venta/ComprobantePagoDAO.java
@@ -2,9 +2,10 @@
 package pe.edu.pucp.frutilla.crud.dao.venta;
 
 import pe.edu.pucp.frutilla.models.venta.ComprobantePago;
-import java.sql.SQLException;
+
 import java.util.ArrayList;
 
+import pe.edu.pucp.frutilla.crud.dao.BaseDAO;
 public interface ComprobantePagoDAO extends BaseDAO<ComprobantePago> {
 
     // Métodos extras
