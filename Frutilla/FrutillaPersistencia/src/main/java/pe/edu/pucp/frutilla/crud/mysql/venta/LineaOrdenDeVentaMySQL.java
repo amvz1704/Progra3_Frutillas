445c42f0b61703b543664fe5627751ce0cabad4a
Lspartan12 <a20200948@pucp.edu.pe>
2025-05-12 00:31:03 -0500

    Agrego un cambito

--- Frutilla/FrutillaPersistencia/src/main/java/pe/edu/pucp/frutilla/crud/mysql/venta/LineaOrdenDeVentaMySQL.java
+++ Frutilla/FrutillaPersistencia/src/main/java/pe/edu/pucp/frutilla/crud/mysql/venta/LineaOrdenDeVentaMySQL.java
@@ -15,6 +15,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import pe.edu.pucp.frutilla.crud.mysql.BaseDAOImpl;
 
