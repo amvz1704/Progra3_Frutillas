d2c4453ea41dcb87f00f0f801840ebbd2263fcac
amvz1704 <amvz17042013@gmail.com>
2025-06-04 03:27:05 -0500

    FE

--- FrutillaFront/FrutillaLocalWebService/LocalWebService/MainLayout.Master.designer.cs
+++ FrutillaFront/FrutillaLocalWebService/LocalWebService/MainLayout.Master.designer.cs
@@ -51,6 +51,15 @@ namespace LocalWebService
         protected global::System.Web.UI.WebControls.HyperLink hlHome;
 
         /// <summary>
+        /// Control HyperLink2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
+
+        /// <summary>
         /// Control HyperLink1.
         /// </summary>
         /// <remarks>
