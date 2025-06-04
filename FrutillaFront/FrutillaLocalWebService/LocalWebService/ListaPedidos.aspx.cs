4ce54efd26a38d2a641d629f8385ae6ad9301360
Lspartan12 <a20200948@pucp.edu.pe>
2025-06-04 03:05:07 -0500

    front

--- FrutillaFront/FrutillaLocalWebService/LocalWebService/ListaPedidos.aspx.cs
+++ FrutillaFront/FrutillaLocalWebService/LocalWebService/ListaPedidos.aspx.cs
@@ -1,5 +1,4 @@
-﻿using LocalWebService.PedidoWS;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,31 +9,9 @@ namespace LocalWebService
 {
     public partial class ListaPedidos : System.Web.UI.Page
     {
-        protected PedidoWSClient cliente;
-        protected void Page_Init(object sender, EventArgs e)
-        {
-            
-            PedidoWSClient cliente = new PedidoWSClient();
-        }
         protected void Page_Load(object sender, EventArgs e)
         {
-        }
 
-        protected void btnBuscar_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                int idEmpleado = int.Parse(txtIdEmpleado.Text);
-                
-                var pedidos = cliente.obtenerPedidosPorEmpleado(idEmpleado);
-                gvPedidos.DataSource = pedidos;
-                gvPedidos.DataBind();
-                lblMensaje.Text = "";
-            }
-            catch (Exception ex)
-            {
-                lblMensaje.Text = "Error: " + ex.Message;
-            }
         }
     }
 }
